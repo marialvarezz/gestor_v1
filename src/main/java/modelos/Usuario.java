f121263948174cac16d39151042060b4d3235ceb
marialvarezz <123487313+marialvarezz@users.noreply.github.com>
2023-02-28 17:48:02 +0100

    Añadida clase genérica Usuario

---
+++ src/main/java/modelos/Usuario.java
@@ -0,0 +1,14 @@
+package modelos;
+
+import java.util.Date;
+
+public class Usuario {
+    private Long id;
+    private String emilio;
+    private String nombre;
+    private String clave;
+    private String rol;
+    private String foto;
+    private Boolean activo = false;
+    private Date creadoEl;
+}
